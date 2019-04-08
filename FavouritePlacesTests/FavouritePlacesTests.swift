fc4bc2f6b3ea03728012ad43dc011b10126c6505
Kane Clerke <kane.clerke@gmail.com>
2019-04-08 17:49:42 +1000

    Initial Commit

---
+++ FavouritePlacesTests/FavouritePlacesTests.swift
@@ -0,0 +1,36 @@
+//
+//  FavouritePlacesTests.swift
+//  FavouritePlacesTests
+//
+//  Created by Kane Clerke on 8/4/19.
+//  Copyright © 2019 Kane Clerke. All rights reserved.
+//
+
+import XCTest
+@testable import FavouritePlaces
+
+class FavouritePlacesTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
